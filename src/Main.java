0970101bf3b5976eb5545d36774d9463eac44e18
Daniyar <pssghoil@gmail.com>
2025-03-06 11:11:23 +0500

    fix(Connection): One connection for all voids

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
-import org.checkerframework.checker.units.qual.C;
-
-import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -64,101 +61,98 @@ public class Main {
     private static final String password = "1234";
     public static void main(String[] args) throws SQLException {
         Scanner scanner = new Scanner(System.in);
-        // Проверка на подключение базы
-        // Если нет подключения то выведит ошибку
-        // В противоположно случаем программа будет работать дальше
-        connectDataBase(URL, userName, password); // функция для проверки подключения к базе
-        while (true){
-            System.out.println("PRESS [1] TO ADD USER"); // Добавить пользоваетля
-            System.out.println("PRESS [2] TO LIST USERS"); // Список пользоваетлей
-            System.out.println("PRESS [3] TO DELETE USER"); // Удалить пользоваетля
-            System.out.println("PRESS [4] TO UPDATE USER"); // Обновить пользоваетля
-            System.out.println("PRESS [5] TO EXIT"); // Выход из программы
-            int choice = scanner.nextInt();
-            scanner.nextLine();
 
-            switch (choice) {
-                case 1:
-                    System.out.print("Введите имя пользователя: ");
-                    String userName = scanner.nextLine();
-                    System.out.print("Введите почту: ");
-                    String email = scanner.nextLine();
-                    System.out.print("Введите пароль: ");
-                    String password = scanner.nextLine();
-                    if(userName.isEmpty() || email.isEmpty() || password.isEmpty()){
-                        System.out.println("Одно из значении было пусто. Введите еще раз");
-                        continue;
-                    }else{
-                        User user = new User(null, userName,email, password);
-                        insertRecordUser(user);
-                        break;
-                    }
-                case 2:
-                    System.out.println("\n=== Список пользователей ===");
-                    ArrayList<User> users = getUsers();
-                    if (users.isEmpty()) {
-                        System.out.println("Список пуст");
-                    } else {
-                        for (User u : users) {
-                            System.out.println("id: " + u.getId() + ", Имя пользователя: " + u.getUsername() + ", почта: " + u.getEmail());
-                        }
-                    }
-                    break;
-                case 3:
-                    System.out.println("Ввдите индентифкационный номер пользователя (id): ");
-                    try {
-                        int idUser = Integer.parseInt(scanner.nextLine());
-                        if (idUser > 0) {
-                            deleteUser(idUser);
-                        } else {
-                            System.out.println("ID должен быть положительным числом!");
-                        }
-                    } catch (NumberFormatException e) {
-                        System.out.println("Введите корректное число!\n" + e.getMessage());
-                    }
-                    break;
-                case 4:
-                    System.out.println("Введите индентифкационный номер пользователя (id) которого вы хотите обновить: ");
-                    try{
-                        int idUpdateUser = Integer.parseInt(scanner.nextLine());
-                        if(!findUserById(idUpdateUser)){
-                            System.out.println("Пользователь с ID " + idUpdateUser + " не найден!");
+        try (Connection connection = DriverManager.getConnection(URL, userName, password)){
+            while (true){
+                System.out.println("PRESS [1] TO ADD USER"); // Добавить пользоваетля
+                System.out.println("PRESS [2] TO LIST USERS"); // Список пользоваетлей
+                System.out.println("PRESS [3] TO DELETE USER"); // Удалить пользоваетля
+                System.out.println("PRESS [4] TO UPDATE USER"); // Обновить пользоваетля
+                System.out.println("PRESS [5] TO EXIT"); // Выход из программы
+                int choice = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (choice) {
+                    case 1:
+                        System.out.print("Введите имя пользователя: ");
+                        String userName = scanner.nextLine();
+                        System.out.print("Введите почту: ");
+                        String email = scanner.nextLine();
+                        System.out.print("Введите пароль: ");
+                        String password = scanner.nextLine();
+                        if(userName.isEmpty() || email.isEmpty() || password.isEmpty()){
+                            System.out.println("Одно из значении было пусто. Введите еще раз");
+                            continue;
+                        }else{
+                            User user = new User(null, userName,email, password);
+                            insertRecordUser(user, connection);
                             break;
                         }
-                        System.out.println("Пользователь найден!\n");
-                        System.out.print("Введите новое имя пользователя: ");
-                        String newUserName = scanner.nextLine();
-                        System.out.print("Введите новую почту: ");
-                        String newEmail = scanner.nextLine();
-                        System.out.print("Введите новый пароль: ");
-                        String NewPassword = scanner.nextLine();
-
-                        if (newUserName.isEmpty() || newEmail.isEmpty() || NewPassword.isEmpty()) {
-                            System.out.println("Поля не могут быть пустыми!");
-                        }else{
-                            updateUser(idUpdateUser, newUserName, newEmail, NewPassword);
+                    case 2:
+                        System.out.println("\n=== Список пользователей ===");
+                        ArrayList<User> users = getUsers(connection);
+                        if (users.isEmpty()) {
+                            System.out.println("Список пуст");
+                        } else {
+                            for (User u : users) {
+                                System.out.println("id: " + u.getId() + ", Имя пользователя: " + u.getUsername() + ", почта: " + u.getEmail());
+                            }
                         }
-                    }catch (NumberFormatException e){
-                        System.out.println("Введите число!\n" + e.getMessage());
-                    }
-                    break;
-                case 5:
-                    System.out.println("Выход");
-                    return;
-                default:
-                    System.out.println("Невеный выбор! Попробуйте снова");
+                        break;
+                    case 3:
+                        System.out.println("Ввдите индентифкационный номер пользователя (id): ");
+                        try {
+                            int idUser = Integer.parseInt(scanner.nextLine());
+                            if (idUser > 0) {
+                                deleteUser(idUser, connection);
+                            } else {
+                                System.out.println("ID должен быть положительным числом!");
+                            }
+                        } catch (NumberFormatException e) {
+                            System.out.println("Введите корректное число!\n" + e.getMessage());
+                        }
+                        break;
+                    case 4:
+                        System.out.println("Введите индентифкационный номер пользователя (id) которого вы хотите обновить: ");
+                        try{
+                            int idUpdateUser = Integer.parseInt(scanner.nextLine());
+                            if(!findUserById(idUpdateUser, connection)){
+                                System.out.println("Пользователь с ID " + idUpdateUser + " не найден!");
+                                break;
+                            }
+                            System.out.println("Пользователь найден!\n");
+                            System.out.print("Введите новое имя пользователя: ");
+                            String newUserName = scanner.nextLine();
+                            System.out.print("Введите новую почту: ");
+                            String newEmail = scanner.nextLine();
+                            System.out.print("Введите новый пароль: ");
+                            String NewPassword = scanner.nextLine();
+
+                            if (newUserName.isEmpty() || newEmail.isEmpty() || NewPassword.isEmpty()) {
+                                System.out.println("Поля не могут быть пустыми!");
+                            }else{
+                                updateUser(idUpdateUser, newUserName, newEmail, NewPassword, connection);
+                            }
+                        }catch (NumberFormatException e){
+                            System.out.println("Введите число!\n" + e.getMessage());
+                        }
+                        break;
+                    case 5:
+                        System.out.println("Выход");
+                        return;
+                    default:
+                        System.out.println("Невеный выбор! Попробуйте снова");
+                }
             }
         }
     }
 
 
-    private static ArrayList<User> getUsers() throws SQLException {
+    private static ArrayList<User> getUsers(Connection connection) throws SQLException {
         ArrayList<User> users = new ArrayList<>();
         final String query = "select * from users";
-        try(Connection connection = DriverManager.getConnection(URL, userName, password)){
-            Statement statement = connection.createStatement();
+        try(Statement statement = connection.createStatement()){
             ResultSet resultSet = statement.executeQuery(query);
-
             while (resultSet.next()){
                 Long id = resultSet.getLong("id");
                 String name = resultSet.getString("username");
@@ -173,11 +167,10 @@ public class Main {
     }
 
 
-    private static void deleteUser(int index)throws SQLException{
+    private static void deleteUser(int index, Connection connection)throws SQLException{
         final String queryFind = "select * from users where id = ?";
         final String query = "delete from users where id = ?";
-        try(Connection connection = DriverManager.getConnection(URL, userName, password)){
-            PreparedStatement preparedStatementFind = connection.prepareStatement(queryFind);
+        try(PreparedStatement preparedStatementFind = connection.prepareStatement(queryFind)){
             preparedStatementFind.setInt(1, index);
             try(ResultSet resultSet = preparedStatementFind.executeQuery()){
                 if(resultSet.next()){
@@ -195,21 +188,18 @@ public class Main {
 
     }
 
-    private static boolean findUserById(int id)throws SQLException{
-        final String CHECK_USER_UPDATE = "Select COUNT(*) from users where id = ?";
-        try(Connection connection = DriverManager.getConnection(URL, userName, password);
-            PreparedStatement preparedStatement = connection.prepareStatement(CHECK_USER_UPDATE)){
+    private static boolean findUserById(int id, Connection connection)throws SQLException{
+        final String check_user_update = "Select COUNT(*) from users where id = ?";
+        try(PreparedStatement preparedStatement = connection.prepareStatement(check_user_update)){
             preparedStatement.setInt(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
 
             return resultSet.next() && resultSet.getInt(1) > 0;
         }
     }
-    private static void updateUser(int id, String newUsername, String NewUserEmail, String newUserPassword)throws SQLException{
+    private static void updateUser(int id, String newUsername, String NewUserEmail, String newUserPassword, Connection connection)throws SQLException{
         final String query = "UPDATE users SET username = ?, email = ?, password = ? WHERE id = ?";
-        try(Connection connection = DriverManager.getConnection(URL, userName, password)){
-            PreparedStatement statement = connection.prepareStatement(query);
-
+        try(PreparedStatement statement = connection.prepareStatement(query)){
             statement.setString(1, newUsername);
             statement.setString(2, NewUserEmail);
             statement.setString(3, newUserPassword);
@@ -221,12 +211,11 @@ public class Main {
         }
     }
 
-    private static void insertRecordUser(User user)throws SQLException{
+    private static void insertRecordUser(User user, Connection connection)throws SQLException{
         final String CHECK_USER_SQL = "SELECT COUNT(*) FROM users WHERE username = ? OR email = ?";
         final String INSERT_SQL_USER = "INSERT INTO users (username, email, password) VALUES (?, ?, ?);";
 
-        try(Connection connection = DriverManager.getConnection(URL, userName, password)){
-            PreparedStatement preparedStatementCheckToDublicate = connection.prepareStatement(CHECK_USER_SQL);
+        try(PreparedStatement preparedStatementCheckToDublicate = connection.prepareStatement(CHECK_USER_SQL);){
             preparedStatementCheckToDublicate.setString(1, user.getUsername());
             preparedStatementCheckToDublicate.setString(2, user.getEmail());
             try(ResultSet resultSet = preparedStatementCheckToDublicate.executeQuery()){
